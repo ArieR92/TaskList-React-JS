01358f901503c7af67cbd92fe0887ff2c013aa92
Ariel <arielrodriguez0@outlook.com>
2021-11-22 01:12:22 -0600

    tercer commit

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaCheckCircle, FaPencilAlt, FaTimes} from "react-icons/fa";
 
 const  Tarea = ({tarea}) => {
+const [editandoTarea, cambiarEditandoTarea] = useState(false);
+const [nuevaTarea, cambiarNuevaTarea] = useState(tarea.texto);
+const handleSubmit = (e) => {e.preventDefault(); cambiarEditandoTarea(false)};
+
+
     return (
         
         <li  className="lista-tareas__tarea">
             <div className="lista-tareas__texto">
                 <FaCheckCircle className="lista-tareas__icono lista-tareas__icono-check"/>
-                {tarea.texto}
+                {editandoTarea ? 
+                <form action="" className="formulario-editar-tarea" onSubmit={handleSubmit}>
+                    <input type="text" className="formulario-editar-tarea__input" value={nuevaTarea} onChange={(e) => cambiarNuevaTarea(e.target.value)}/>
+                    <button type="submit" className="formulario-editar-tarea__btn">Actualizar</button>
+
+                </form>
+                :
+                tarea.texto
+                }
+                
+
             </div>
 
 
             <div className="lista-tareas__contenedor-botones">
-                    <FaPencilAlt  className="lista-tareas__icono lista-tareas__icono-accion"/>
+                    <FaPencilAlt  className="lista-tareas__icono lista-tareas__icono-accion" onClick={() => cambiarEditandoTarea (!editandoTarea)}/>
                     <FaTimes  className="lista-tareas__icono lista-tareas__icono-accion"/>
 
             </div>
